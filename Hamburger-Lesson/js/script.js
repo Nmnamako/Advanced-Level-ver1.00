549a3b65b58dcbea32de4b3fc09cd1793a5f6241
EC2 Default User <na.maechan@icloud.com>
2022-12-15 04:17:05 +0000

    [add]Javascript10章 途中経過

--- Hamburger-Lesson/js/script.js
+++ Hamburger-Lesson/js/script.js
@@ -1,8 +1,7 @@
 $(function() {
-  $('#back a').on('click',function(event){  // #back内のaタグがクリックされたときの処理
-    $('body, html').animate({  //$('body, html')はページ全体に対して処理を実行したいから//.animateはアニメーション効果を設定するjQueryの関数
-      scrollTop:0  //変化対象のプロパティ名:変化値
-    }, 800);  //アニメーションの動作時間,slow,normal,fastでもok,800は0.8秒になる。
-    event.preventDefault();  //event.preventDefault();は、aタグの機能を無効にする。画面遷移を持っているが今回は不要のため
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefailt();
   });
 });
