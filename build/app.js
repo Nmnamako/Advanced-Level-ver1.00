f9303e88f18c939951c6f3063c43573f9e374df8
EC2 Default User <na.maechan@icloud.com>
2022-12-27 06:13:30 +0000

    [Update]ボールの描画と動きの追加完了

--- build/app.js
+++ build/app.js
@@ -3,29 +3,53 @@ const canvas = document.getElementById("myCanvas");
 //2D描画コンテキスト,実際に絵を描画するためのツール
 const ctx = canvas.getContext("2d");
 
+//constで試すと壁への衝突時にボールが停止する
+//ここでは毎フレーム描画させた時の位置を書いている
+//新たに描画されたらその描画されたところを起点に書いていく
+let dx = 2;
+let dy = -2;
+
+//ボールの半径を指定,計算で指定するため
+const ballRadius = 10;
 
 
+//ボールを配置する指示 canvas.width / 2 で最大高さを割る2して配置
+//canvas,height -30 で中心から下へ配置
+//functionより上に記述したほうがいい？
 
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+function drawBall() {
+  ctx.beginPath();
+  
+  //x,y二はfaunction上部に記述した変数が読み込まれている
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
 
+function draw() {
+  //clearRect()で古い描画を削除している
+  //四角形の左上端の x、y 座標と四角形の右下端の x、y 座標で削除範囲を指定
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  
+  if(y + dy > canvas.height | y + dy < 0) {
+    dy = -dy;
+  }
+  
+  if(x + dx > canvas.width | x + dx < 0) {
+    dx = -dx;
+  }
+  
+  //+=でdx,dyを足すことによって毎フレーム描画する位置を変えている
+  x += dx;
+  y += dy;
+}
+//setInterval()は同じ関数を何度も実行できる
+//今回の場合はdrawを10ミリ秒ごとに実行される
+//このタイミング関数を使うとdrawが永遠に描き続けられる
+//そのため動きを付けないと何重にも書き足されていく
+setInterval(draw, 10);
 
-//四角を描画
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);  //四角の形状と位置の指定 (X,Y,W,H)
-ctx.fillStyle = "#FF0000";  //色を指定
-ctx.fill();  //サブパスを塗りつぶす,色指定だけでは塗られない？
-ctx.closePath(); //サブパスを閉じている,三角だと線三つ目を閉じる様な感じ
-
-//丸い絵を描画
-ctx.beginPath();
-//arc(中心x座標, 中心y座標, 半径, 円弧の開始角, 円弧の終了角, 描画の方向)
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-//塗りつぶし四角
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
