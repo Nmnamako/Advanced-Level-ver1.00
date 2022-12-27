00f0cc749875ec5cc6965b5c1a74fa98f44bdfa4
EC2 Default User <na.maechan@icloud.com>
2022-12-27 08:42:42 +0000

    [Update]パドルの追加,操作可能にするためのif構文完成

--- build/app.js
+++ build/app.js
@@ -6,6 +6,16 @@ const ctx = canvas.getContext("2d");
 //ボールの半径を指定,計算で指定するため
 const ballRadius = 10;
 
+//ボールを跳ね返すパドルを用意する
+const paddleHeight = 10;
+const paddleWidth = 75;
+
+//パドルのx軸の開始地点を定義している
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+//パドルを操作するための記述
+let rightPressed = false
+let leftPressed = false
 
 //constで試すと壁への衝突時にボールが停止する
 //ここでは毎フレーム描画させた時の位置を書いている
@@ -31,29 +41,81 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   //clearRect()で古い描画を削除している,これでボールが動いている様に見える
   //四角形の左上端の x、y 座標と四角形の右下端の x、y 座標で削除範囲を指定
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-  
-  //もしボールの位置のyの値が0未満だったら、符号判定させた値を設定することで、
+  drawPaddle();
+  //もしボールの位置のyの値が0未満だったら、符号反転させた値を設定することで、
   //y軸方向の動きの向きを変える
   //yとdyを足した時にcanvasの高さより大きくなってしまったら-dyを入れて向きを変える
   //y+dyが０より小さくなったら-dyで逆方向にいくので跳ね返りが表現される
-  if(y + dy > canvas.height | y + dy < 0) {
+  //条件分岐内にballRadiusを入れると玉の中心ではなく境界で反射する
+  if(y + dy > canvas.height-ballRadius | y + dy < ballRadius) {
     dy = -dy;
   }
   
-  if(x + dx > canvas.width | x + dx < 0) {
+  if(x + dx > canvas.width-ballRadius | x + dx < ballRadius) {
     dx = -dx;
   }
+  
+  //パドル操作
+  //rightPressedg押されていた場合+7される
+  //&&以降はcanvas内からパドルが出ないように指示している
+  if(rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  }
+  else if (leftPressed && paddleX > 0){
+    paddleX += -7;
+  }
+  
   
   //+=でdx,dyを足すことによって毎フレーム描画する位置を変えている
   //ここで初めてボールが動き始める
   x += dx;
   y += dy;
 }
+
+//setInterval()の上に記述するとのこと
+//ここで押したの有無でkeyDownHandlerが実行される、二つ目も同様
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+
+
+function keyDownHandler(e){
+  //e.keyはカラムみたいなもの、RightもしくはArrowRightが押されたら
+  //trueを返す、keyUpHandlerも同様
+  //キーを押された時、その情報を変数に保存する
+  //ArrowRightはキーボードの矢印
+  //RightやLeftはIE/Edgeなどに対応するため入れている
+  
+  if (e.key == "Right" || e.key == "ArrowRight" ){
+    rightPressed = true
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft" ){
+    leftPressed = true
+  }
+}
+
+function keyUpHandler(e){
+  if (e.key == "Right" || e.key == "ArrowRight" ){
+    rightPressed = false
+  }
+  else if (e.key == "Left" || e.key == "ArrowLeft" ){
+    leftPressed = false
+  }
+}
+
 //setInterval()は同じ関数を何度も実行できる
 //今回の場合はdrawを10ミリ秒ごとに実行される
 //このタイミング関数を使うとdrawが永遠に描き続けられる
