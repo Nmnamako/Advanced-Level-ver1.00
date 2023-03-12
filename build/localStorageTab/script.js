d367a7a2bf346b68c05e515a09482de7b8638a34
EC2 Default User <na.maechan@icloud.com>
2023-03-12 06:20:36 +0000

    [Update]タブ切り替え保存途中経過

--- build/localStorageTab/script.js
+++ build/localStorageTab/script.js
@@ -1,26 +1,50 @@
 const change1 = 1;
 let changeLater = 0;
 
+let tab1 = document.getElementsByClassName('tab_class')[0];
+let tab2 = document.getElementsByClassName('tab_class')[1];
+let tab3 = document.getElementsByClassName('tab_class')[2];
+let tab4 = document.getElementsByClassName('tab_class')[3];
 
 //localStorage.setItem("lstest", changeLater)
 //console.log(localStorage.getItem("lstest"))
 
-document.getElementById("test1").addEventListener('click', function() {
-  //document.getElementById("change1").textContent = "クリック";
-  //document.getElementById("change1").textContent = localStorage.getItem('lstest');
-  if (document.getElementById("change1").innerHTML == 0) {
-    changeLater = 1;
-    localStorage.setItem("lstest", changeLater);
-    localStorage.saveKey = changeLater;
-    document.getElementById("change1").textContent = localStorage.getItem("lstest");
-    console.log(localStorage.getItem("lstest"));
-  } else {
-    changeLater = 0;
-    localStorage.setItem("lstest", changeLater);
-    localStorage.saveKey = changeLater;
-    document.getElementById("change1").textContent = localStorage.getItem("lstest");
-    console.log(localStorage.getItem("lstest"));
-  };
-});
 
-document.getElementById("change1").textContent = localStorage.getItem("lstest");
+
+// document.getElementsByClassName("tab_class")[0].addEventListener('click', function() {
+//   if (tab1.inputElement.checked) {
+//     localStorage.setItem("check", changeLater);
+//   };
+// });
+
+
+
+// document.getElementById("test1").addEventListener('click', function() {
+//   //document.getElementById("change1").textContent = "クリック";
+//   //document.getElementById("change1").textContent = localStorage.getItem('lstest');
+//   if (document.getElementById("change1").innerHTML == 0) {
+//     changeLater = 1;
+//     localStorage.setItem("lstest", changeLater);
+//     localStorage.saveKey = changeLater;
+//     document.getElementById("change1").textContent = localStorage.getItem("lstest");
+//     console.log(localStorage.getItem("lstest"));
+//   } else {
+//     changeLater = 0;
+//     localStorage.setItem("lstest", changeLater);
+//     localStorage.saveKey = changeLater;
+//     document.getElementById("change1").textContent = localStorage.getItem("lstest");
+//     console.log(localStorage.getItem("lstest"));
+//   };
+// });
+
+// document.getElementById("change1").textContent = localStorage.getItem("lstest");
+
+
+let test1 = document.getElementsByClassName("test")[0];
+let test2 = document.getElementsByClassName("test")[1];
+
+let btnA = document.getElementsByClassName("btnA")[0];
+let btnB = document.getElementsByClassName("btnB")[0];
+
+
+// console.log(test1);
