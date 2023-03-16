fa5cbd2300938559f916e4ccedafec2d786de9dd
EC2 Default User <na.maechan@icloud.com>
2023-03-16 08:32:18 +0000

    [Update]5章終了

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
   
   post 'lists' => 'lists#create'
   
   get '/top' => "homes#top"
+  get 'lists/index' => "lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
